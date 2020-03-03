53b1b7b932ecb99acc8dc564fe97c18a79c04201
Anthony Zheng <zhengant@gmail.com>
2020-03-03 12:21:02 -0800

    replace tanh with relu, add sigmoid into model

--- model.py
+++ model.py
@@ -5,12 +5,12 @@ class VAEEncoder(torch.nn.Module):
     def __init__(self, input_size, hidden_size, latent_size):
         super(VAEEncoder, self).__init__()
         self.fc1 = torch.nn.Linear(input_size, hidden_size)
-        self.tanh1 = torch.nn.Tanh()
+        self.relu = torch.nn.ReLU()
         self.fc_mean = torch.nn.Linear(hidden_size, latent_size)
         self.fc_logvar = torch.nn.Linear(hidden_size, latent_size)
 
     def forward(self, x):
-        hidden = self.tanh1(self.fc1(x))
+        hidden = self.relu(self.fc1(x))
         mean = self.fc_mean(hidden)
         logvar = self.fc_logvar(hidden)
 
@@ -21,12 +21,14 @@ class VAEDecoder(torch.nn.Module):
     def __init__(self, latent_size, hidden_size, output_size):
         super(VAEDecoder, self).__init__()
         self.fc1 = torch.nn.Linear(latent_size, hidden_size)
-        self.tanh1 = torch.nn.Tanh()
+        self.relu = torch.nn.ReLU()
         self.fc2 = torch.nn.Linear(hidden_size, output_size)
+        self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, x):
-        hidden = self.tanh1(self.fc1(x))
+        hidden = self.relu(self.fc1(x))
         output = self.fc2(hidden)
+        output = self.sigmoid(output)
 
         return output
 
@@ -51,7 +53,7 @@ class VAE(torch.nn.Module):
         eps = torch.randn((batch_size, num_samples, self.latent_size)).to(self.device)
         z = (
             mean.view((batch_size, 1, self.latent_size))
-            + logvar.view((batch_size, 1, self.latent_size)).exp() * eps
+            + (0.5*logvar.view((batch_size, 1, self.latent_size))).exp() * eps
         )
 
         output = self.decoder(z)
@@ -64,9 +66,14 @@ def vae_loss(x, xhat, mean, logvar):
     kl_term = -0.5 * torch.sum((1 + logvar - torch.pow(mean, 2) - logvar.exp()))
 
     likelihood_term = 0
-    bce = torch.nn.BCEWithLogitsLoss()
+    bce = torch.nn.BCELoss(reduction="sum")
     num_samples = xhat.shape[1]
+    batch_size = xhat.shape[0]
     for i in range(num_samples):
         likelihood_term += bce(xhat[:, i, :], x.view(-1, x.shape[1]))
 
-    return kl_term + likelihood_term / num_samples, kl_term, likelihood_term
+    return (
+        (kl_term + (likelihood_term / num_samples)) / batch_size,
+        kl_term,
+        likelihood_term,
+    )
